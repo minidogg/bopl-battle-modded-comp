7c7f4aaa93d9cfb4c6bc61f46311bdd4cd746170
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-06 16:17:02 -0600

    making the help command way over engineered

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -1,11 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
+const help = {
+    "ping":"Replies with pong"
+}
+var helpString = "Here are the available commands:\n"
+for(let i = 0;i<Object.keys(help).length;i++){
+    helpString+="/"+Object.keys(help)[i]+" - " +help[Object.keys(help)[i]]+ "\n"
+}
+
 const helpEmbed = {
     color: 0x0099ff,
     title: 'Command List',
-    description: "Here are the available commands:\n"+(+[
-        "/ping - Replies with pong",
-        "/account stats - Gets your stats or optionally you can specify another player's username."
-    ].join("\n"))
+    description: helpString
 };
 
 module.exports = {
