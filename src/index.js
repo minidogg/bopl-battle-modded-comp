8ad6296e2c3b083013237005d6b73bba39a0f786
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-06 18:43:05 -0600

    E

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-function startServer(port,botData,mode="dev"){
-    new Promise((resolve,reject)=>{
-
-    const fs = require("fs")
-    var {bot}=require("./bot/index.js")
-    var {express}=require("./express/index.js")
-
-    try{fs.readFileSync("./accounts.json")}catch{fs.writeFileSync("./accounts.json",JSON.stringify({accounts:{}}))}
-
-    try{
-        var botObj = bot(botData)
-        botObj.send('Server up! Being hosted by '+botData.host+'. In mode: '+mode,"1215079919151743047",0x32CD32)
-
-        express(port,botObj)
-    }catch(err){
-        console.warn(err)
-        resolve(err)
-    }
-
-})
-   
-}
-module.exports.startServer = startServer
-
 const botData = require("./bot.json")
-startServer(3000,botData)
+const {startServer} = require("./run.js")
+startServer(3000,botData,"production")
--- src/prod.js
+++ src/prod.js
@@ -1,4 +1,6 @@
-const botData = require("./bot.json")
-const {startServer} = require("./index.js")
-
-startServer(3000,botData,"production")
+(async()=>{
+    const botData = require("./bot.json")
+    const {startServer} = require("./run.js")
+    console.warn(await startServer(3000,botData,"production"))
+    console.log("e")
+})()
