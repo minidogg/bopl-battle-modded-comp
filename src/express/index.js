c5bfbac3a6d7a72bfd86b7299668199ae221844c
WackyModer <78763021+WackyModer@users.noreply.github.com>
2024-03-06 23:26:33 -0800

    mmm express server go yummy

--- src/express/index.js
+++ src/express/index.js
@@ -2,7 +2,12 @@ module.exports.express = (port,botObj)=>{
     const express = require('express')
     const app = express()
     
-    app.use(express.static("/public"))
+    // now exposes everything in the 'public' folder.
+    // so if you go to index.html it will show it.
+    // theres no path traversal with .. so we're safe on that front.
+    // static pages WILL NOT work for db access
+    // we would have to use the router for that
+    app.use(express.static("./express/public"))
     botObj.send("Express.js server up!","1215079919151743047")
     
     app.listen(port, () => {
