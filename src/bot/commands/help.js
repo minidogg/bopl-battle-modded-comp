f624362920a323f3d936c59fb22a0a8698e123b3
BuildTools <unconfigured@null.spigotmc.org>
2024-03-07 12:20:51 -0700

    updared help

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -1,8 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
 const help = {
-    "help":{short:"Sends a list of commands and what they do. Optionally you can specifiy the command you want a description of."},
-    "ping":{short:"Replies with pong",long:'A very sophisticated command that hastily replies to your well written message with "pong"'}
-}
+    "help": { short: "Sends a list of commands and what they do. Optionally you can specify the command you want a description of." },
+    "ping": { short: "Replies with pong", long: 'A very basic command that replies to your message with "pong".' },
+    "game": { short: "stats a game lobby", long: 'Starts a game lobby for you to start playing.' },
+    "leaderboard": { short: "Displays the leaderboard", long: 'Shows the leaderboard of top players.' },
+    "account": { short: "Manages your account", long: 'Manages your user account.' },
+    "account create": { short: "Creates a new account", long: 'Creates a new user account.' },
+    "account delete": { short: "Deletes your account", long: 'Deletes your user account.' },
+    "account stats": { short: "Displays account statistics", long: 'Shows statistics related to your user account.' }
+};
+
 var helpString = "Here are the available commands:\n"
 for(let i = 0;i<Object.keys(help).length;i++){
     helpString+="/"+Object.keys(help)[i]+" - " +help[Object.keys(help)[i]].short+ "\n"
